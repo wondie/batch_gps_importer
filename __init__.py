ce78d87dd42145ab19a4902ad15cc70362e31a36
Wondimagegn Tesfaye Beshah <wondim81@gmail.com>
2018-09-20 23:18:32 -0500

    Updated plugin path

--- __init__.py
+++ __init__.py
@@ -9,7 +9,8 @@ from os.path import expanduser
 HOME = expanduser("~")
 LOCALE = QSettings().value("locale/userLocale")[0:2]
 PLUGIN_FOLDER = 'batch_gps_importer'
-PLUGIN_DIR = '{}/.qgis2/python/plugins/{}'.format(HOME, PLUGIN_FOLDER)
+PLUGIN_DIR = '{}/AppData/Roaming/QGIS/QGIS3/profiles/default/python/plugins/{}'.\
+  format(HOME, PLUGIN_FOLDER)
 if LOCALE.startswith('en_'):
   LOCALE = 'en'
 DYNAMIC_HELP = '{}/help/dynamic/{}'.format(PLUGIN_DIR, LOCALE)
